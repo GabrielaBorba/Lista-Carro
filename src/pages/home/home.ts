b91be7fd5765410bdcf406d6acc46d2953e7651f
gabrielaborba <gabi_borba@hotmail.com>
2018-04-18 20:05:20 -0300

    Primeiro Commit Lista Carros

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public carros;
 
+  constructor(public navCtrl: NavController) {
+    this.carros=[
+      {nome:"Jaguar", preco: 200000},
+      {nome:"Range Rover", preco: 50000},
+      {nome:"Chevrolet", preco:20000},
+      {nome:"Peugeout", preco:15000},
+      {nome:"Renault", preco:10000},
+    ];
   }
 
 }
